dd82a2f01f4e5a474ead8abb7b12de38de5fee93
Jordan Kamin <jordanakamin@gmail.com>
2017-09-06 16:06:19 -0700

    adds some functionality

--- node.rb
+++ node.rb
@@ -1,2 +1,12 @@
 class Node
+  attr_reader :data, :left
+  def initialize(arguments)
+    @data = arguments[:data]
+    @left = nil
+  end
+
+  def create_child(data)
+    child = Node.new({data: data})
+    @left = child
+  end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,10 +2,31 @@ require 'rspec'
 require_relative '../node'
 
 describe Node do
+  let(:node) { Node.new({data: 5}) }
   describe 'attributes' do
-    it 'has data'
-    it 'has a left child'
+    it 'has data' do
+      expect(node.data).to eq(5)
+    end
+    xit 'has a left child' do
+      expect(node.left).to_not be_nil
+    end
     it 'has a right child'
     it 'has a parent'
+  end
+  describe '#create_child' do
+    it 'chooses left or right'
+    it 'creates a child with the passed in data' do
+      child = node.create_child(3)
+      expect(child.data).to eq(3)
+    end
+    it 'sets the left attribute to the new child' do
+      child = node.create_child(3)
+      expect(node.left).to eq(child)
+    end
+    context 'raises an exception when' do
+      it 'the right child exists and child data is greater than the parent data'
+      it 'the left child exists and child data is less than the parent data'
+      it 'there are already two children'
+    end
   end
 end
