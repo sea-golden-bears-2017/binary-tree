eaa1d3d6122240f3ff9ff451b65290aeacc4db40
Jordan Kamin <jordanakamin@gmail.com>
2017-09-06 15:28:51 -0700

    builds empty its for node

---
+++ spec/node_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require_relative '../node'
+
+describe Node do
+  describe 'attributes' do
+    it 'has data'
+    it 'has a left child'
+    it 'has a right child'
+    it 'has a parent'
+  end
+end
